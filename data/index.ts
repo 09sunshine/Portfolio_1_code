0b19709da1d4dcb1ce5066c5dda1ffee651926b0
09sunshine <sanshey_230456@aitpune.edu.in>
2025-06-25 20:40:39 +0530

    new feature

--- data/index.ts
+++ data/index.ts
@@ -39,7 +39,7 @@ export const gridItems = [
   {
     id: 4,
     title: "Tech enthusiast with a passion for development and design.",
-    description: "I'm Batman :)",
+    description: "I'm Batman ^.^",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
