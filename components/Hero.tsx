1d547479dc6ad364a5c6302db1b262f7527f0d43
Aditi-207 <aditishelar2004@gmail.com>
2025-06-25 20:21:19 +0530

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,47 @@
+
+import { FaLocationArrow } from 'react-icons/fa'
+import MagicButton from './ui/MagicButton'
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-15">
+       
+        <div>
+            <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
+            <Spotlight className="top-10 left-full h-[100vh] w-[50vh]" fill="purple"/>
+            <Spotlight className="top-28 left-80 h-[80vh] w-[50vh]" fill="blue"/>
+        </div>
+      
+        
+       
+        <div className='flex flex-col justify-center items-center z-10 relative my-20'>
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vh] flex flex-col items-center justify-center'>
+                <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+                    [Let&apos;s work together]
+                </h2>
+            </div>
+        
+          <TextGenerateEffect 
+          className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          words='Transforming Ideas into Impactful Digital Experiences'/>
+
+          <p className='text-center md:tracking-wider mt-5 mb-4 text-sm md:text-lg lg:text-xl'>
+            Hi, I&apos;m Sanshey — a <span className='text-purple-300'>Full-Stack Web Developer</span> & <span className='text-purple-300'>UI/UX Designer.</span>{' '}
+            I build robust, scalable web solutions that are as delightful to use as they are powerful.
+          </p>
+          <a href='#projects'>
+            <MagicButton 
+            title="Show my work" icon={<FaLocationArrow />} 
+            position='right'/>
+            
+          </a>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
---
+++ data/index.ts
@@ -0,0 +1,213 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development and design.",
+    description: "I'm Batman :)",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Hi, I'm Sanshey! {' '} <h1>Engineer by day Hustler by night</h1>",
+    description: "[About Me]",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Enculture AI",
+    des: "Enculture.ai – Engineered an AI‑powered culture transformation platform that helps organizations measure, visualize, and act on employee engagement through advanced analytics, customizable surveys, and real‑time feedback loops.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://www.enculture.ai/",
+  },
+  {
+    id: 2,
+    title: "Brainwave",
+    des: "Brainwave – Developed an AI-powered chat application using Next.js and advanced natural language processing to streamline productivity and communication across devices.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://brainwave-pi-nine.vercel.app/",
+  },
+  
+  {
+    id: 3,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://i-phone15-pro-six.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Sanshey has a solid ability to understand briefs and deliver good design and product thinking on tight deadlines. As founders we're very demanding and Sanshey was able to more than keep up with the pace, he is an asset for any design and digital product team.",
+    name: "Anshul Tibrewala",
+    title: "Co-founder and COO of Snipe",
+  },
+  {
+    quote:
+      "Sanshey brings clarity to chaos—his ability to transform complex ideas into elegant, user-centric interfaces is exceptional. He deeply understands user journeys and consistently delivers high-quality design under pressure. Whether it’s tight timelines or shifting priorities, Sanshey adapts fast and delivers work that not only looks great but solves real problems. A true asset to any product or design team.",
+    name: "Anuradha Deswani",
+    title: "Senior Product Marketing Manager -Enculture AI",
+  },
+  {
+    quote:
+      "Sanshey is a builder at heart. His full-stack capabilities allowed us to move quickly and confidently through our development cycle. He’s not just technically sound but also product-minded—he proactively suggests better ways to implement features and always keeps the end-user in mind. If you're looking for someone who can code, think, and ship like a pro, Sanshey is your guy.",
+    name: "Era Gupta",
+    title: "Founder of Paaltu",
+  },
+  {
+    quote:
+      "Working with Sanshey feels like having a full product team in one person. From frontend polish to backend robustness, he delivers with precision and speed. His ability to take a product from concept to deployment—while maintaining clean, scalable code—is rare. We’ve thrown complex challenges at him, and he’s handled them all with maturity and technical depth.",
+    name: "Yuvraj Jwala",
+    title: "Director and Project Manager of MMG | Ex-Software Engineer @Swiggy",
+  },
+  
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "MongoDB",
+    img: "/mongo.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "NextJS",
+    img: "/next.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "ReactJS",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "NodeJS",
+    img: "/node.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "ExpressJS",
+    img: "/express.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "UI/UX Designer - Snipe",
+    dur: "Oct 2024 - Feb 2025",
+    desc: "Assisted in delivering various design projects using Figma, Framer and Web Flow.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "UI/UX Designer - MMG",
+    dur: "July 2024 - Sept 2024",
+    desc: "Worked as the lead designer in various projects including websites, app and dashboards.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "UI/UX Designer - Paaltu",
+    dur: "Sept 2024 - Nov 2024",
+    desc: "Worked closely with the founders to develop a pet and human community application.",
+    className: "md:col-span-2", 
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Looking for Work",
+    dur: "Hire Me Now",
+    desc: "Elevate your organization with design-driven development — let’s connect.",
+    className: "md:col-span-2", 
+    thumbnail: "/hire.svg",
+  },
+  
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/09sunshine",
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/09_s.unshine/",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/sanshey-017b23292/",
+  },
+];
